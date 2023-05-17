41f2201c6a1057696517ceacaf57ed8947f89bed
GiftonNdemo <67891176+GiftonNdemo@users.noreply.github.com>
2023-05-17 17:33:05 +0300

    Update settings.py

--- Tetakenya/Tetakenya/settings.py
+++ Tetakenya/Tetakenya/settings.py
@@ -148,5 +148,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'giftonndemoosindi@gmail.com'
-EMAIL_HOST_PASSWORD = 'Morasta_6'
+EMAIL_HOST_USER = 'someEmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'passWAD'
